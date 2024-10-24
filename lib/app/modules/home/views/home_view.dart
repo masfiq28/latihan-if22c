a11099c51477ce5f3c9b1d1eb023e64f25d04eca
syafiqnasrullah2802 <syafiqnasrullah2802@gmail.com>
2024-10-24 09:12:15 +0000

    third commit

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,28 +9,134 @@ class HomeView extends GetView<HomeController> {
   final cAuth = Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
+    return DashboardAdmin();
+  }
+}
+
+class DashboardAdmin extends StatefulWidget {
+  const DashboardAdmin({super.key});
+
+  @override
+  State<DashboardAdmin> createState() => _DashboardAdminState();
+}
+
+class _DashboardAdminState extends State<DashboardAdmin> {
+  final cAuth = Get.find<AuthController>();
+  int _index = 0;
+  List<Map> _fragment = [
+    {'title': 'Dashboard', 'view': 'Dashboard'},
+    {'title': 'Data Mahasiswa', 'view': 'Mahasiswa'},
+    {'title': 'Data Dosen', 'view': 'Dosen'},
+    {'title': 'Data Pegawai', 'view': 'Pegawai'}
+  ];
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      drawer: drawer(),
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'HomeView are working exceptionally',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20), // Spacer
-            ElevatedButton(
-              onPressed: () {
-                cAuth.logout(); 
-              },
-              child: const Text('Logout'),
-            ),
-          ],
+        backgroundColor: Colors.indigo,
+        titleSpacing: 0,
+        title: Text(_fragment[_index]['title'],
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget drawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.indigo,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.account_circle,
+                    size: 80,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "M Syafiq Nasrullah",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    'Admin',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              )),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 0);
+              Get.back();
+            },
+            leading: Icon(Icons.dashboard),
+            title: Text('Dashboard'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.indigo,
+            textColor: Colors.indigo,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 1);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Mahasiswa'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.indigo,
+            textColor: Colors.indigo,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 2);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Dosen'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.indigo,
+            textColor: Colors.indigo,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 3);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Pegawai'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.indigo,
+            textColor: Colors.indigo,
+          ),
+          ListTile(
+            onTap: () {
+              Get.back();
+              cAuth.logout();
+            },
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.indigo,
+            textColor: Colors.indigo,
+          ),
+        ],
       ),
     );
   }
